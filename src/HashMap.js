823990b6126acd623c6926c96ae290945fcfeaa6
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-05 04:43:45 +0000

    Implement project structure

---
+++ src/HashMap.js
@@ -0,0 +1,16 @@
+export default class HashMap {
+  constructor() {
+    this.loadFactor = 0.75;
+    this.capacity = 16;
+  }
+  hash = (key) => {};
+  set = (key, value) => {};
+  get = (key) => {};
+  has = (key) => {};
+  remove = (key) => {};
+  length = () => {};
+  clear = () => {};
+  keys = () => {};
+  values = () => {};
+  entries = () => {};
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import HashMap from "./HashMap";
+
+console.log();
